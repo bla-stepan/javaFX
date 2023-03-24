b265c3149eb2c84fcb853e6e12d5398aa3177684
Stepan <bla-stepan@mail.ru>
2023-03-24 14:25:47 +0300

    Пример ToggleButton пакет JavaFXExample

--- src/main/java/JavaFXExample/JavaFXExample.java
+++ src/main/java/JavaFXExample/JavaFXExample.java
@@ -7,8 +7,14 @@ import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.CornerRadii;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
 import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
 import javafx.stage.Stage;
 
 //Примеры кода ComboBox, TaggleButton, RadioButton, Spinner на панели TabPane
@@ -17,12 +23,13 @@ public class JavaFXExample extends Application {
     public void start(Stage primaryStage) throws Exception {
         //создание элемента Tab с названием и панелью с элементами
         Tab tabComboBox = new Tab("Пример ComboBox", comboBoxExample());
+        Tab tabToggleButton = new Tab("Пример ToggleButton", toggleButtonExample());
 
         //создание TabPane
         TabPane examples = new TabPane();
 
         //добавляем вкладки в Tab
-        examples.getTabs().addAll(tabComboBox);
+        examples.getTabs().addAll(tabComboBox, tabToggleButton);
 
         //создаем заголовок
         primaryStage.setTitle("Примеры визуальных компонентов JavaFX");
@@ -34,6 +41,10 @@ public class JavaFXExample extends Application {
         primaryStage.show();
     }
 
+    //значения параметров единого стиля шрифта
+    int elementHeightSize = 25;
+    int elementWidthSize = 300;
+    Font font = Font.font("Tahoma", 15);
     //для копонентов вкладки comboBox создаем панель с вертикальным расположением элементов
     private VBox comboBoxExample() {
         //для comboBox создаем список с значениями (опциями)
@@ -54,14 +65,11 @@ public class JavaFXExample extends Application {
         comboBox.setPrefSize(300, 25);
         comboBox.setStyle("-fx-font-size: 15px");//для комбобок стиль задается так
 
-        //значения параметров
-        int elementHeightSize = 25;
-        Font font = Font.font(15);
-
         //сздаем метку
         Label label = new Label();
         label.setFont(font);
         label.setPrefSize(400, elementHeightSize);
+        label.setAlignment(Pos.CENTER);
 
         //создаем кнопку для демонстрации работы
         Button button = new Button("Пример ComboBox");
@@ -89,6 +97,45 @@ public class JavaFXExample extends Application {
         vBoxComboBox.setSpacing(10);
         vBoxComboBox.getChildren().addAll(comboBox, button, label);
         return vBoxComboBox;
+    }
+
+    //для компонентов вкладки toggleButton создаем панель VBox с компонентами
+    private VBox toggleButtonExample(){
+        //создаем параметры шрифта
+
+        //создаем метку
+        Label result = new Label();
+        result.setFont(font);
+        result.setPrefSize(elementWidthSize, elementHeightSize);
+        result.setAlignment(Pos.CENTER);
+
+        //создаем кнопку переключатель
+        ToggleButton toggleButton = new ToggleButton("ToggleButton");
+        toggleButton.setFont(font);
+        toggleButton.setPrefSize(elementWidthSize, elementHeightSize);
+        toggleButton.setOnAction(event -> {
+            //проверяем включена ли кнопка
+            if (toggleButton.isSelected()) {
+                //меняем цвет поля метки
+                result.setText("ВКЛЮЧЕНО");
+                result.setTextFill(Color.BLACK);
+                result.setBackground(new Background(new BackgroundFill(Color.GREEN, CornerRadii.EMPTY, Insets.EMPTY)));
+                result.setFont(Font.font("Tahoma", FontWeight.BOLD, 15));
+            }else {
+                result.setText("ВЫКЛЮЧЕНО");
+                result.setTextFill(Color.INDIGO);
+                result.setBackground(new Background(new BackgroundFill(Color.AQUA, CornerRadii.EMPTY, Insets.EMPTY)));
+                result.setFont(Font.font("Tahoma", FontWeight.LIGHT, 15));
+            }
+        });
+
+        VBox vBoxToggleButton = new VBox();
+        vBoxToggleButton.setPadding(new Insets(10, 100, 10, 100));
+        vBoxToggleButton.setSpacing(10);
+        vBoxToggleButton.setAlignment(Pos.CENTER);
+        vBoxToggleButton.getChildren().addAll(toggleButton, result);
+
+        return vBoxToggleButton;
     }
 
     public static void main(String[] args) {
